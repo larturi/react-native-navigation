7fc7d943f6e273f379be4052bdd167b58e5ed871
lea.arturi <lea.arturi@gmail.com>
2021-08-16 12:43:09 -0300

    SideBar

--- App.tsx
+++ App.tsx
@@ -4,12 +4,14 @@ import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 
-import {StackNavigator} from './src/navigator/StackNavigator';
+// import {StackNavigator} from './src/navigator/StackNavigator';
+import {SideBarBasic} from './src/navigator/SideBarBasic';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <SideBarBasic />
+      {/* <StackNavigator /> */}
     </NavigationContainer>
   );
 };
